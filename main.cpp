f25a1b2f2cc8a4f2cceacc64d7f9a16e8088e07e
Tonssikas <150654164+Tonssikas@users.noreply.github.com>
2023-11-13 16:32:19 +0200

    Ensiversio
    Hotellihuoneen varausohjelman ensiversio

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+
+
+
+int numberInput(int low, int high){
+    bool valid {false};
+    while (!valid) {
+        int input;
+        std::cin >> input;
+        if (std::cin.fail()){
+            std::cout << "Syote ei kelpaa, anna luku valilta " << low << " - " << high << "\n";
+            std::cin.clear();
+            std::cin.ignore(100, '\n');
+            continue;
+        }
+
+        if (input >= low && input <= high){
+            return input;
+        }
+        else {
+            std::cout << "Luku ei kelpaa, syota luku valilta " << low << " - " << high << "\n";
+            continue;
+        }
+    }
+}
+
+
+
+int main() {
+    bool Suorita = true;
+    const int huoneidenLKM {30};
+    int vapaat = huoneidenLKM;
+    int Huoneet [huoneidenLKM];
+    std::fill_n(Huoneet,huoneidenLKM,1);
+    int yoLKM {0};
+
+while (Suorita) {
+    std::cout << "Vapaiden huoneiden lukumaara: " << vapaat << "\n";
+
+    std::cout<<"Mita haluat tehda? \n"
+    <<"1. Varaa huone\n"
+    <<"2. Poistu\n";
+
+    switch(numberInput(1,2)) {
+        case 1: {
+            bool varattu = true;
+            while(varattu) {
+                std::cout << "Minka huoneen haluat varata? (Numero) ";
+                int varattavaHuone = numberInput(1, 30);
+                if (Huoneet[varattavaHuone - 1] == 1) {
+                    std::cout << "Huone on vapaa, sen hinta on 100e per yo.\n";
+                    std::cout << "Monekstiko yoksi haluat varta huoneen? ";
+                    yoLKM = numberInput(1, 100);
+                    std::cout << "\nVarauksen hinta on " << yoLKM * 100 << " euroa." << "\n";
+
+                    vapaat = vapaat - 1;
+                    Huoneet[varattavaHuone - 1] = 0;
+                    varattu = false;
+                } else {
+                    std::cout<< "Huone on jo varattu. \n";
+                }
+
+            }
+
+            break;
+        }
+
+        case 2: {
+            Suorita = false;
+            break;
+        }
+    }
+}
+    return 0;
+}
